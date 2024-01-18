5085781c719790e22de59713803b95a44c8a3350
Maria Vitória Amorim Farias Barbosa <vitamorim4002@gmail.com>
2024-01-17 21:36:51 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sobre from "./Coomponents/Sobre.js";
 import Projetos from "./Coomponents/Projetos.js";
@@ -8,13 +8,13 @@ import Certificados from "./Coomponents/Certificados.js";
 function App() {
   return (
    <div className="App">
-    <HashRouter>
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Sobre/>} />
       <Route path="/Projetos" element={<Projetos/>} />
       <Route path="/Certificados" element={<Certificados/>} />
     </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </div>
   );
 }
