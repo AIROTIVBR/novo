2e67c2bf256a84b306507c056ea726b8d948caec
Maria Vitória Amorim Farias Barbosa <vitamorim4002@gmail.com>
2024-01-17 21:50:21 -0300

    teste2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { Switch, Route} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sobre from "./Coomponents/Sobre.js";
 import Projetos from "./Coomponents/Projetos.js";
-// import Certificados from "./Coomponents/Certificados.js";
+import Certificados from "./Coomponents/Certificados.js";
 
 function App() {
   return (
    <div className="App">
-    <Switch>
-    <Route exact path="/">
-    <Sobre />
-    </Route>
-    <Route path="/Projetos">
-    <Projetos />
-    </Route>
-</Switch>
+    <Routes>
+      <Route path="/" element={<Sobre/>} />
+      <Route path="/Projetos" element={<Projetos/>} />
+      <Route path="/Certificados" element={<Certificados/>} />
+    </Routes>
     </div>
   );
 }
