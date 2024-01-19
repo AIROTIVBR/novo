3e76d0e64587c5fd05807dd35f4cb18d9686a8e0
Maria Vitória Amorim Farias Barbosa <vitamorim4002@gmail.com>
2024-01-19 19:22:59 -0300

    adição do ícone da página,alteração do nome da página e atualização do conteúdo

--- src/Coomponents/Sobre.js
+++ src/Coomponents/Sobre.js
@@ -39,12 +39,10 @@ import './assets/style/style.css';
                 </Col>
                 <Col xs lg="6" className="sobre">
                     <h3 className="titulo">Sobre mim</h3>
-                    <p className="text">Atualmente, tenho 19 anos e estou cursando o quinto semestre de Análise e Desenvolvimento de Sistemas (ADS) na Unifavip Wyden.
-                     Sou uma estudante dedicada com um pensamento crítico e estou constantemente buscando maneiras de aprimorar minhas habilidades.
-                     Estou ativamente procurando oportunidades de trabalho na área de T.I, onde espero combinar meu aprendizado com experiências práticas para meu desenvolvimento profissional.
-                     Tenho um forte desejo de adquirir conhecimento e contribuir para projetos significativos na sociedade.
-                     Estou particularmente interessada em áreas como ciência de dados e engenharia de software, pretendo me aprimorar nessas áreas ao longo deste ano.
-                     Estou ansiosa para poder aplicar meu conhecimento e habilidades em projetos relevantes, enquanto continuo minha jornada de aprendizado e crescimento profissional.</p>
+                    <p className="text">Tenho 19 anos, cursando o quinto semestre de Análise e Desenvolvimento de Sistemas na Unifavip Wyden. 
+                    Sou estudante dedicada com pensamento crítico, buscando oportunidades na área de T.I. Quero combinar aprendizado com experiências práticas, 
+                    especialmente em ciência de dados e engenharia de software. Meu objetivo é contribuir para projetos significativos, aplicando conhecimentos e 
+                    habilidades enquanto continuo minha jornada de aprendizado e crescimento profissional.</p>
                     <Link to="https://linktr.ee/vitoriamorim" target="blank" rel="noopener noreferrer">
                             <button className="button">
                                 <span className="button-content">Linktree</span>
@@ -68,7 +66,7 @@ import './assets/style/style.css';
                     <h4 className="exp">Estagiária em dados</h4>
                     <p className="text-exp">
                         Atualmente tenho o cargo de estagiária em dados no Hospital Unimed Caruaru, minha atividade envolve desenvolver aplicações para a otimização
-                         e modernização de diversos processos. Até o momento utilizamos Python, SQL, Git, Excel, pandas, numpy, flask, matplotlib, plotly e dash, 
+                         e modernização de diversos processos, disponibilizando. Até o momento utilizamos Python, SQL, Git, Excel, pandas, numpy, flask, matplotlib, plotly e dash, 
                          além da metodologia ágil de Scrum.
                          Comecei nesse cargo em de Setembro de 2023 e sigo atualmente.
                     </p>
@@ -79,9 +77,9 @@ import './assets/style/style.css';
                     <h4 className="exp">Analista de qualidade</h4>
                     <p className="text-exp">
                         Estou atuando como analista de qualidade em um projeto voluntário para a Secretaria de políticas para mulheres de Caruaru, 
-                        nosso objetivo é desenvolver do zero uma aplicação web para digitalização de processos da secretaria aderindo á metodologia ágil do Scrum.
-                        Meu trabalho envolve a realização de testes manuais unitários, funcionais e de regressão, além de ter auxiliado os Product Owners responsáveis
-                        na análise de requisitos e no planejamento de sprints. Estou nesse cargo desde Abril de 2023 e sigo atualmente.
+                        nosso objetivo é desenvolver do zero uma aplicação web para digitalização de processos da secretaria aderindo a metodologia ágil do Scrum.
+                        Meu trabalho envolve a criação de casos e cenários de teste, o planejamento e a realização de testes manuais unitários, funcionais e de regressão, além de ter auxiliado os Product Owners responsáveis
+                        na análise de requisitos e no planejamento de sprints, utilizamos as plataformas do trello e também do jira. Estou nesse cargo desde Abril de 2023 e sigo atualmente.
                     </p>
                 </Col>
                 <Col xs lg="4">
@@ -107,7 +105,7 @@ import './assets/style/style.css';
                         <li><p className="skills">Pensamento analítico</p></li>
                         <li><p className="skills">Sempre inovando</p></li>
                         <li><p className="skills">Proatividade</p></li>
-                        <li><p className="skills">Comunicação eficaz</p></li>
+                        <li><p className="skills">Comunicação clara e objetiva</p></li>
                         <li><p className="skills">Criticidade</p></li>
                         <li><p className="skills">Sempre aprendendo</p></li>
                         <li><p className="skills">Dedicação</p></li>
@@ -117,17 +115,15 @@ import './assets/style/style.css';
                     <h2 className="tituloskills">Hard skills</h2>
                     <div className="espc">
                     <p className="minitil">Meus pontos fortes são:</p>
-                    <p className="skills">Python e algumas de suas bibliotecas e frameworks(Pandas, Flask, Numpy, plotly, dash) e focando em no setor de qualidade a 
-                    escrita de casos de teste e a realização de testes manuais (unitário, black-box, regressão, funcionais). Tenho familiaridade com o Git e com as metodologias
-                    ágeis Scrum e Kanban.</p>
+                    <p className="skills">Python(Pandas, Flask, Numpy, plotly, dash). Escrita de casos e cenários de teste e a realização de testes manuais 
+                    (unitário, black-box, regressão, funcionais). Tenho familiaridade com o Git e com as metodologias ágeis Scrum e Kanban.</p>
                     <br></br>
                     <p className="minitil">Conheço e me dou bem com:</p>
-                    <p className="skills">HTML, CSS, React e Boostrap na área de web, me sinto confiante com Bancos de dados relacionais(MySQL) e 
-                    Bancos não relacionais(MongoDB).Assim como Power BI para a análise exploratória de dados.</p>
+                    <p className="skills">HTML, CSS, React e Boostrap, Bancos de dados relacionais(MySQL) e 
+                    Bancos não relacionais(MongoDB) e as ferramentas Power BI, Jira e Trello.</p>
                     <br></br>
                     <p className="minitil">Sou capaz de dar auxílio em:</p>
-                    <p className="skills">Java e Spring boot. Tenho conhecimentos teóricos
-                     bem fundamentados na área de nuvem principalmente dos serviços da AWS,não possuo muita experiência. </p>
+                    <p className="skills">Java e Spring boot. Área de computação em nuvem principalmente dos serviços da AWS. </p>
                     </div>
                 </Col>
                 <Row className="footer"></Row>
